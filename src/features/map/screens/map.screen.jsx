b75768c82d72c8cddd012725ac215d14806fdadf
tee2490@hotmail.com <tee2490@hotmail.com>
2022-11-10 15:44:27 +0700

    Setting The Map Region

--- src/features/map/screens/map.screen.jsx
+++ src/features/map/screens/map.screen.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
 import { Search } from "../components/search.component";
 import styled from "styled-components/native";
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
 const Map = styled(MapView)`
   height: 100%;
@@ -9,8 +11,34 @@ const Map = styled(MapView)`
 `;
 
 export default function MapScreen() {
-  return  <>
-  <Search />
-  <Map />
-</>
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
+  return (
+    <>
+      <Search />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return null;
+        })}
+      </Map>
+    </>
+  );
 }

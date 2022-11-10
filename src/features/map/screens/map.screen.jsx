682a1665a8f72f14d9df38a18bb0a8b8f4181e6b
tee2490@hotmail.com <tee2490@hotmail.com>
2022-11-10 16:12:05 +0700

    Rendering Map Markers

--- src/features/map/screens/map.screen.jsx
+++ src/features/map/screens/map.screen.jsx
@@ -36,7 +36,16 @@ export default function MapScreen() {
         }}
       >
         {restaurants.map((restaurant) => {
-          return null;
+          return (
+            <MapView.Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
+            />
+          );
         })}
       </Map>
     </>

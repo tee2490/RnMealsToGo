794468d5412265b4f3969bbce8923c46e7fbfe91
tee2490@hotmail.com <tee2490@hotmail.com>
2022-11-11 11:20:28 +0700

    Custom Map Callout Solution

--- src/features/map/screens/map.screen.jsx
+++ src/features/map/screens/map.screen.jsx
@@ -11,7 +11,7 @@ const Map = styled(MapView)`
   width: 100%;
 `;
 
-export default function MapScreen() {
+export default function MapScreen({ navigation }) {
   const { location } = useContext(LocationContext);
   const { restaurants = [] } = useContext(RestaurantsContext);
 
@@ -46,7 +46,11 @@ export default function MapScreen() {
                 longitude: restaurant.geometry.location.lng,
               }}
             >
-              <MapView.Callout>
+              <MapView.Callout  onPress={() =>
+                  navigation.navigate("RestaurantDetail", {
+                    restaurant,
+                  })
+                }>
                 <MapCallout restaurant={restaurant} />
               </MapView.Callout>
             </MapView.Marker>

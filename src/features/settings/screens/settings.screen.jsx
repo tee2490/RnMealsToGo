9ad0e07df1ac4366cb83393c0070644f45298108
tee2490@hotmail.com <tee2490@hotmail.com>
2022-11-16 17:10:08 +0700

    Setting Up The Camera Infrastructure

--- src/features/settings/screens/settings.screen.jsx
+++ src/features/settings/screens/settings.screen.jsx
@@ -5,6 +5,7 @@ import { AuthenticationContext } from "../../../services/authentication/authenti
 import styled from "styled-components/native";
 import { Text } from "../../../components/typography/text.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
+import { TouchableOpacity } from "react-native";
 
 const SettingsItem = styled(List.Item)`
   padding: ${(props) => props.theme.space[3]};
@@ -19,7 +20,9 @@ export const SettingsScreen = ({ navigation }) => {
   return (
     <SafeArea>
       <AvatarContainer>
-        <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+        <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
+          <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+        </TouchableOpacity>
         <Spacer position="top" size="large">
           <Text variant="label">{user.email}</Text>
         </Spacer>

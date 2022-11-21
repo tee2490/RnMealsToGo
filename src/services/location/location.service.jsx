6f09e31b191e7dd41e9b9fd69e362c3b3ab69764
tee2490@hotmail.com <tee2490@hotmail.com>
2022-11-21 15:07:42 +0700

    Moving Location Mock Part 2

--- src/services/location/location.service.jsx
+++ src/services/location/location.service.jsx
@@ -1,8 +1,19 @@
 import camelize from "camelize";
 
-export const locationRequest = (searchTerm) => { };
+const host = "https://0914-202-28-123-199.ap.ngrok.io"
+
+export const locationRequest = (searchTerm) => {
+  return fetch(
+    `${host}/mealstogo-ba2ae/us-central1/helloWorld?city=${searchTerm}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((e) => console.log(e));
+};
 
 export const locationTransform = (result) => {
+  console.log(result);
   const formattedResponse = camelize(result);
   const { geometry = {} } = formattedResponse.results[0];
   const { lat, lng } = geometry.location;

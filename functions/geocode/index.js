7bbb04c0f980ee30078c0a6f91fd71aec28355b8
tee2490@hotmail.com <tee2490@hotmail.com>
2022-11-23 18:01:58 +0700

    Setting Up Google Node SDK

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,9 +1,28 @@
 const {locations : locationsMock} = require("./geocode.mock")
 const url = require("url");
 
-module.exports.geocodeRequest = (request,response)=>{
-    const {city}=url.parse(request.url,true).query;
+module.exports.geocodeRequest = (request,response,client)=>{
+    const {city,mock}=url.parse(request.url,true).query;
     const locationMock=locationsMock[city.toLowerCase()];
 
-    response.json(locationMock)
+    if (mock === "true") {
+        const locationMock = locationsMock[city.toLowerCase()];
+        return response.json(locationMock);
+      }
+
+      client
+      .geocode({
+        params: {
+          address: city,
+          key: "apikey for google",
+        },
+        timeout: 1000,
+      })
+      .then((res) => {
+        return response.json(res.data);
+      })
+      .catch((e) => {
+        response.status(400);
+        return response.send(e.response.data.error_message);
+      });
 }

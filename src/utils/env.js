d6e06499feaac49553d2c6778f076f86d11ee0c8
tee2490@hotmail.com <tee2490@hotmail.com>
2022-11-24 14:57:23 +0700

    Integrating Places API

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,7 +1,8 @@
 const liveHost = "https://us-central1-mealstogo-ba2ae.cloudfunctions.net";
-const baseUrl = "https://50ae-223-24-93-95.ap.ngrok.io";
+const baseUrl = "https://245f-202-28-123-196.ap.ngrok.io";
 const localHost = `${baseUrl}/mealstogo-ba2ae/us-central1`
 
 export const isDevelopment = process.env.NODE_ENV === "development";
 
 export const host = isDevelopment ? localHost : liveHost;
+//export const host = liveHost;

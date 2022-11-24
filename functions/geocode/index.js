a7faf44d4d19ac7e46a8f269a1ed9884eac7ad60
tee2490@hotmail.com <tee2490@hotmail.com>
2022-11-24 11:16:20 +0700

    Integrating Geocoding API

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,28 +1,30 @@
-const {locations : locationsMock} = require("./geocode.mock")
+const { locations: locationsMock } = require("./geocode.mock");
 const url = require("url");
+const functions = require("firebase-functions");
 
-module.exports.geocodeRequest = (request,response,client)=>{
-    const {city,mock}=url.parse(request.url,true).query;
-    const locationMock=locationsMock[city.toLowerCase()];
+module.exports.geocodeRequest = (request, response, client) => {
+  const { city, mock } = url.parse(request.url, true).query;
 
-    if (mock === "true") {
-        const locationMock = locationsMock[city.toLowerCase()];
-        return response.json(locationMock);
-      }
+  if (mock === "true") {
+    const locationMock = locationsMock[city.toLowerCase()];
+    return response.json(locationMock);
+  }
 
-      client
-      .geocode({
-        params: {
-          address: city,
-          key: "apikey for google",
-        },
-        timeout: 1000,
-      })
-      .then((res) => {
-        return response.json(res.data);
-      })
-      .catch((e) => {
-        response.status(400);
-        return response.send(e.response.data.error_message);
-      });
-}
+  client
+    .geocode({
+      params: {
+        address: city,
+        key: functions.config().google.key,
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      return response.json(res.data);
+    })
+    .catch((e) => {
+      response.status(400);
+      var t = functions.config().test;
+      console.log(JSON.stringify(t));
+      return response.send(e.response.data.error_message);
+    });
+};
--- src/services/location/location.service.jsx
+++ src/services/location/location.service.jsx
@@ -10,8 +10,8 @@ export const locationRequest = (searchTerm) => {
 };
 
 export const locationTransform = (result) => {
-  console.log(result);
   const formattedResponse = camelize(result);
+  console.log(result);
   const { geometry = {} } = formattedResponse.results[0];
   const { lat, lng } = geometry.location;
 

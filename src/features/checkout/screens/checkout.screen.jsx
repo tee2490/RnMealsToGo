1403405794ae524cd69575a38dbe9578668f0c3b
tee2490@hotmail.com <tee2490@hotmail.com>
2022-11-28 17:59:28 +0700

    Our Credit Card Input

--- src/features/checkout/screens/checkout.screen.jsx
+++ src/features/checkout/screens/checkout.screen.jsx
@@ -1,3 +1,9 @@
 import React from "react";
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { CreditCardInput } from "../components/credit-card.component";
 
-export const CheckoutScreen = () => null;
+export const CheckoutScreen = () => (
+  <SafeArea>
+    <CreditCardInput />
+  </SafeArea>
+);

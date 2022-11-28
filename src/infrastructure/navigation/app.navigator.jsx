5fdbbc9d57eb3f20374dd80895171c9a7fa7d8cf
tee2490@hotmail.com <tee2490@hotmail.com>
2022-11-28 10:58:50 +0700

    Setting Stripe Client and Checkout

--- src/infrastructure/navigation/app.navigator.jsx
+++ src/infrastructure/navigation/app.navigator.jsx
@@ -8,12 +8,14 @@ import { SettingsNavigator } from "./settings.navigator";
 import { RestaurantsContextProvider } from "../../services/restaurants/restaurants.context";
 import { LocationContextProvider } from "../../services/location/location.context";
 import { FavouritesContextProvider } from "../../services/favourites/favourites.context";
+import { CheckoutScreen } from "../../features/checkout/screens/checkout.screen";
 
 const Tab = createBottomTabNavigator();
 
 const TAB_ICON = {
   Restaurants: "md-restaurant",
   Map: "md-map",
+  Checkout: "md-cart",
   Settings: "md-settings",
 };
 
@@ -35,6 +37,7 @@ export const AppNavigator = () => (
       <RestaurantsContextProvider>
         <Tab.Navigator screenOptions={createScreenOptions}>
           <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
+          <Tab.Screen name="Checkout" component={CheckoutScreen} />
           <Tab.Screen name="Map" component={MapScreen} />
           <Tab.Screen name="Settings" component={SettingsNavigator} />
         </Tab.Navigator>
--- src/utils/env.js
+++ src/utils/env.js
@@ -1,9 +1,9 @@
 const liveHost = "https://us-central1-mealstogo-ba2ae.cloudfunctions.net";
-const baseUrl = "https://37a9-202-28-123-199.ap.ngrok.io";
+const baseUrl = "https://59d7-202-28-123-199.ap.ngrok.io";
 const localHost = `${baseUrl}/mealstogo-ba2ae/us-central1`
 
 export const isDevelopment = process.env.NODE_ENV === "development";
 
-export const isMock = false;
+export const isMock = true;
 export const host = isDevelopment ? localHost : liveHost;
 //export const host = liveHost;
